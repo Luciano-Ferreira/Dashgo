045e5bd9e8a903f0a17dc0aa1973af40ff0f54e3
Luciano Ferreira <luciano2023silva@gmail.com>
2021-11-22 21:19:35 -0300

    fixing _document

--- src/pages/_document.ts
+++
@@ -1,9 +0,0 @@
-import Document, { Head, Html, Main, NextScript } from "next/document";
-
-export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html></Html>
-    );
-  }
-}
