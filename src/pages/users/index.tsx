afb49bc2ec16ef76018aa3d44edbfab5be236aca
Luciano Ferreira <luciano2023silva@gmail.com>
2022-01-05 19:34:42 -0300

    add fake api with miragejs and faker

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -5,13 +5,20 @@ import { Pagination } from '../../components/Pagination';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
+import { useEffect } from 'react';
 
 
 export default function UserList() {
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true
-  })
+  });
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/users')
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }, [])
 
   return (
     <Box>
